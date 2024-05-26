2bb3a9196dbaade96b4971976ae01c25d5da0590
Ilya Lyasin <lyasin.i@mail.ru>
2024-05-26 19:08:26 +0300

    add UICollectionView

---
+++ UnsplashPhotoApp/MainModule/View/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  UnsplashPhotoApp
+//
+//  Created by Ilia Liasin on 26/05/2024.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
